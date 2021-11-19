d3e4d263409ca26507602b900eed370814fc463f
LIM QING YUAN <TP046490@mail.apu.edu.my>
2021-11-19 15:50:50 +0800

    Owner controller and index dependency injection

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,15 +1,29 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    //Injected property
+    //@Autowired is not added as after spring 4.2 it knows what do inject
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     //The request mapping annotation above has shortened the mapping
+    //By declaring model, spring mvc will add a model when it calls this method
     @RequestMapping({"/","","/index","/index.html"})
-    public String ListOwners(){
+    public String ListOwners(Model model){
+        //owner attribute will be added and can be referred to in the owner index template
+        model.addAttribute("owners",ownerService.findAll());
+
         return "owners/index";
     }
 }
